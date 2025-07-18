b0a3257d86429a1debe53ca81016a6c87651e6b6
victoralexandersiquiejchajon2004 <siquiejvictor4@gmail.com>
2025-07-18 20:41:42 +0200

    Agrega mensaje final de agradecimiento

--- src/Veterinaria.java
+++ src/Veterinaria.java
@@ -5,10 +5,13 @@ public class Veterinaria {
     Mascota mascota1 = new Mascota ("rufo", "pitbull", 3, true);
     Mascota mascota2 = new Mascota ("canelo", "Labrador", 1, true);
     Mascota mascota3 = new Mascota ("blanca", "pitbull", 6, false);
+    Mascota mascota4 = new Mascota ("firulais", "criollo", 3, false);
+    Mascota mascota5 = new Mascota ("perla", "criollo", 5, true);
     
     mascota1.mostrarInformacion();
     mascota2.mostrarInformacion();
     mascota3.mostrarInformacion();
-    
+    mascota4.mostrarInformacion();
+    mascota5.mostrarInformacion();
 }
 }
